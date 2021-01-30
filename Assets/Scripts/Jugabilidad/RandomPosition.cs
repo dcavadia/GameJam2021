62ba5a469e848cfb7d42f76ef39c72e62677b1c3
dcavadia <dan.acppp@gmail.com>
2021-01-30 16:05:15 -0400

    add bodyparts and fix random positions

--- Assets/Scripts/Jugabilidad/RandomPosition.cs
+++ Assets/Scripts/Jugabilidad/RandomPosition.cs
@@ -7,26 +7,13 @@ public class RandomPosition : MonoBehaviour
 
     //PIEZAS
     [Header("Piezas")]
-    public GameObject pieza1;
-    public GameObject pieza2;
-    public GameObject pieza3;
-    public GameObject pieza4;
-    public GameObject pieza5;
-
+    public GameObject[] bodyParts;
 
     //LOCATIONS
     [Header("Piezas")]
-    public GameObject location1;
-    public GameObject location2;
-    public GameObject location3;
-    public GameObject location4;
-    public GameObject location5;
-    public GameObject location6;
-    public GameObject location7;
-    public GameObject location8;
-    public GameObject location9;
-    public GameObject location10;
+    public GameObject[] hidingLocations;
 
+    static System.Random random = new System.Random();
 
 
     // Start is called before the first frame update
@@ -47,242 +34,42 @@ public class RandomPosition : MonoBehaviour
     // FUNCION PARA ORGANIZAR
     public void Organizar()
     {
-        int cont = 0;
-        while (cont < 5)
+        List<int> vals = GenerateRandom(10);
+        int index = 0;
+
+        foreach (GameObject part in bodyParts)
         {
-            int random = Random.Range(1, 10);
-
-            //........................PIEZA 1....................................
-            if (cont == 0)
-            {
-                if(random == 1)
-                {
-                    GameObject Pieza1 = Instantiate(pieza1, location1.transform.position, location1.transform.rotation);
-                }else if(random == 2)
-                {
-                    GameObject Pieza1 = Instantiate(pieza1, location2.transform.position, location2.transform.rotation);
-                }else if(random == 3)
-                {
-                    GameObject Pieza1 = Instantiate(pieza1, location3.transform.position, location3.transform.rotation);
-                }
-                else if (random == 4)
-                {
-                    GameObject Pieza1 = Instantiate(pieza1, location4.transform.position, location4.transform.rotation);
-                }
-                else if (random == 5)
-                {
-                    GameObject Pieza1 = Instantiate(pieza1, location5.transform.position, location5.transform.rotation);
-                }
-                else if (random == 6)
-                {
-                    GameObject Pieza1 = Instantiate(pieza1, location6.transform.position, location6.transform.rotation);
-                }
-                else if (random == 7)
-                {
-                    GameObject Pieza1 = Instantiate(pieza1, location7.transform.position, location7.transform.rotation);
-                }
-                else if (random == 8)
-                {
-                    GameObject Pieza1 = Instantiate(pieza1, location8.transform.position, location8.transform.rotation);
-                }
-                else if (random == 9)
-                {
-                    GameObject Pieza1 = Instantiate(pieza1, location9.transform.position, location9.transform.rotation);
-                }
-                else if (random == 10)
-                {
-                    GameObject Pieza1 = Instantiate(pieza1, location10.transform.position, location10.transform.rotation);
-                }
-            }
-
-            //..................PIEZA 2..................................................
-
-            if (cont == 1)
-            {
-                if (random == 1)
-                {
-                    GameObject Pieza2 = Instantiate(pieza2, location1.transform.position, location1.transform.rotation);
-                }
-                else if (random == 2)
-                {
-                    GameObject Pieza2 = Instantiate(pieza2, location2.transform.position, location2.transform.rotation);
-                }
-                else if (random == 3)
-                {
-                    GameObject Pieza2 = Instantiate(pieza2, location3.transform.position, location3.transform.rotation);
-                }
-                else if (random == 4)
-                {
-                    GameObject Pieza2 = Instantiate(pieza2, location4.transform.position, location4.transform.rotation);
-                }
-                else if (random == 5)
-                {
-                    GameObject Pieza2 = Instantiate(pieza2, location5.transform.position, location5.transform.rotation);
-                }
-                else if (random == 6)
-                {
-                    GameObject Pieza2 = Instantiate(pieza2, location6.transform.position, location6.transform.rotation);
-                }
-                else if (random == 7)
-                {
-                    GameObject Pieza2 = Instantiate(pieza2, location7.transform.position, location7.transform.rotation);
-                }
-                else if (random == 8)
-                {
-                    GameObject Pieza2 = Instantiate(pieza2, location8.transform.position, location8.transform.rotation);
-                }
-                else if (random == 9)
-                {
-                    GameObject Pieza2 = Instantiate(pieza2, location9.transform.position, location9.transform.rotation);
-                }
-                else if (random == 10)
-                {
-                    GameObject Pieza2 = Instantiate(pieza2, location10.transform.position, location10.transform.rotation);
-                }
-            }
-
-            //..................PIEZA 3..................................................
-
-            if (cont == 2)
-            {
-                if (random == 1)
-                {
-                    GameObject Pieza3 = Instantiate(pieza3, location1.transform.position, location1.transform.rotation);
-                }
-                else if (random == 2)
-                {
-                    GameObject Pieza3 = Instantiate(pieza3, location2.transform.position, location2.transform.rotation);
-                }
-                else if (random == 3)
-                {
-                    GameObject Pieza3 = Instantiate(pieza3, location3.transform.position, location3.transform.rotation);
-                }
-                else if (random == 4)
-                {
-                    GameObject Pieza3 = Instantiate(pieza3, location4.transform.position, location4.transform.rotation);
-                }
-                else if (random == 5)
-                {
-                    GameObject Pieza3 = Instantiate(pieza3, location5.transform.position, location5.transform.rotation);
-                }
-                else if (random == 6)
-                {
-                    GameObject Pieza3 = Instantiate(pieza3, location6.transform.position, location6.transform.rotation);
-                }
-                else if (random == 7)
-                {
-                    GameObject Pieza3 = Instantiate(pieza3, location7.transform.position, location7.transform.rotation);
-                }
-                else if (random == 8)
-                {
-                    GameObject Pieza3 = Instantiate(pieza3, location8.transform.position, location8.transform.rotation);
-                }
-                else if (random == 9)
-                {
-                    GameObject Pieza3 = Instantiate(pieza3, location9.transform.position, location9.transform.rotation);
-                }
-                else if (random == 10)
-                {
-                    GameObject Pieza3 = Instantiate(pieza3, location10.transform.position, location10.transform.rotation);
-                }
-            }
-
-            //..................PIEZA 4..................................................
-
-            if (cont == 3)
-            {
-                if (random == 1)
-                {
-                    GameObject Pieza4 = Instantiate(pieza4, location1.transform.position, location1.transform.rotation);
-                }
-                else if (random == 2)
-                {
-                    GameObject Pieza4 = Instantiate(pieza4, location2.transform.position, location2.transform.rotation);
-                }
-                else if (random == 3)
-                {
-                    GameObject Pieza4 = Instantiate(pieza4, location3.transform.position, location3.transform.rotation);
-                }
-                else if (random == 4)
-                {
-                    GameObject Pieza4 = Instantiate(pieza4, location4.transform.position, location4.transform.rotation);
-                }
-                else if (random == 5)
-                {
-                    GameObject Pieza4 = Instantiate(pieza4, location5.transform.position, location5.transform.rotation);
-                }
-                else if (random == 6)
-                {
-                    GameObject Pieza4 = Instantiate(pieza4, location6.transform.position, location6.transform.rotation);
-                }
-                else if (random == 7)
-                {
-                    GameObject Pieza4 = Instantiate(pieza4, location7.transform.position, location7.transform.rotation);
-                }
-                else if (random == 8)
-                {
-                    GameObject Pieza4 = Instantiate(pieza4, location8.transform.position, location8.transform.rotation);
-                }
-                else if (random == 9)
-                {
-                    GameObject Pieza4 = Instantiate(pieza4, location9.transform.position, location9.transform.rotation);
-                }
-                else if (random == 10)
-                {
-                    GameObject Pieza4 = Instantiate(pieza4, location10.transform.position, location10.transform.rotation);
-                }
-            }
-
-            //..................PIEZA 5..................................................
-
-            if (cont == 4)
-            {
-                if (random == 1)
-                {
-                    GameObject Pieza5 = Instantiate(pieza5, location1.transform.position, location1.transform.rotation);
-                }
-                else if (random == 2)
-                {
-                    GameObject Pieza5 = Instantiate(pieza5, location2.transform.position, location2.transform.rotation);
-                }
-                else if (random == 3)
-                {
-                    GameObject Pieza5 = Instantiate(pieza5, location3.transform.position, location3.transform.rotation);
-                }
-                else if (random == 4)
-                {
-                    GameObject Pieza5 = Instantiate(pieza5, location4.transform.position, location4.transform.rotation);
-                }
-                else if (random == 5)
-                {
-                    GameObject Pieza5 = Instantiate(pieza5, location5.transform.position, location5.transform.rotation);
-                }
-                else if (random == 6)
-                {
-                    GameObject Pieza5 = Instantiate(pieza5, location6.transform.position, location6.transform.rotation);
-                }
-                else if (random == 7)
-                {
-                    GameObject Pieza5 = Instantiate(pieza5, location7.transform.position, location7.transform.rotation);
-                }
-                else if (random == 8)
-                {
-                    GameObject Pieza5 = Instantiate(pieza5, location8.transform.position, location8.transform.rotation);
-                }
-                else if (random == 9)
-                {
-                    GameObject Pieza5 = Instantiate(pieza5, location9.transform.position, location9.transform.rotation);
-                }
-                else if (random == 10)
-                {
-                    GameObject Pieza5 = Instantiate(pieza5, location10.transform.position, location10.transform.rotation);
-                }
-            }
-
-
-            cont = cont + 1;
+            int position = vals[index];
+            Instantiate(part, hidingLocations[position].transform.position, hidingLocations[position].transform.rotation);
+            index++;
         }
 
-        
+    }
+
+    public static List<int> GenerateRandom(int count)
+    {
+        // generate count random values.
+        HashSet<int> candidates = new HashSet<int>();
+        while (candidates.Count < count)
+        {
+            // May strike a duplicate.
+            candidates.Add(random.Next());
+        }
+
+        // load them in to a list.
+        List<int> result = new List<int>();
+        result.AddRange(candidates);
+
+        // shuffle the results:
+        int i = result.Count;
+        while (i > 1)
+        {
+            i--;
+            int k = random.Next(i + 1);
+            int value = result[k];
+            result[k] = result[i];
+            result[i] = value;
+        }
+        return result;
     }
 }
