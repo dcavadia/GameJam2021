487f6d96a474ff2cb344698b4aca0876bec97873
NickScr <62735977+NickScr@users.noreply.github.com>
2021-01-30 14:11:51 -0400

    sfx de puerta y statica incluidos

--- Assets/Scripts/ObjetosEnEscena/DoorController.cs
+++ Assets/Scripts/ObjetosEnEscena/DoorController.cs
@@ -5,19 +5,25 @@ using UnityEngine;
 public class DoorController : MonoBehaviour
 {
     Animator _doorAnim;
+    [SerializeField] public AudioClip open, close;
+    public AudioSource door;
 
     private void OnTriggerEnter(Collider other)
     {
         _doorAnim.SetBool("isOpening", true);
+        door.PlayOneShot(open);
     }
     private void OnTriggerExit(Collider other)
     {
         _doorAnim.SetBool("isOpening", false);
+        door.PlayOneShot(close);
     }
     // Start is called before the first frame update
     void Start()
     {
         _doorAnim = this.transform.parent.GetComponent<Animator>();
+        door = this.transform.parent.GetComponentInParent<AudioSource>();
+        
     }
 
     // Update is called once per frame
