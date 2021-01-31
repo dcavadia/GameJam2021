876a4fcad8df4b17762beb60bac3429a0634df34
Sergio Teran <sergioteran.zambrano@gmail.com>
2021-01-31 02:53:54 -0600

    SERGIO: (WIP) UI game

---
+++ Assets/Scripts/Jugabilidad/TimerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerController : MonoBehaviour
+{
+    [SerializeField] float limit = 3000.0f;
+    [Header("Random SetUp")]
+    [SerializeField] bool randomTime = false;
+    [SerializeField] timeRange range = default;
+    public void StartCount()
+    {
+        Debug.Log("START COUNTER");
+    }
+}
+
+
+[System.Serializable]
+public class timeRange{
+    [SerializeField] float minTime = default;
+    [SerializeField] float maxTime = default;
+}
